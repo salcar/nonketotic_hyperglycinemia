4559be7ec30e0fa319ef08610638463016babad0
Salvador Carlucci <salcar@hotmail.com>
2012-12-10 16:23:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NONKETOTICHyperglycinemia::Application.config.secret_token = 'c112c14cb47641f2d97a859d8c9fda3df840f572a718327d60573d560a29db1f5b4672873746937c3ed5ec4450d24a2bd715b233b8c4c6bffc57469ec538e883'
